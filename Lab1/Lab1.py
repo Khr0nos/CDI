34c8b0919e7071eb256e56a589f7d694e7eb2e3b
Xavi <antiwindows17@gmail.com>
2016-02-20 22:13:36 +0100

    clean_txt done

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -5,22 +5,38 @@ Created on Sat Feb 20 12:45:24 2016
 @author: Javier Garcia Sanchez
 """
 import sys
+import re
+import unicodedata
+
+def strip_accents(text):
+    try:
+        text = unicode(text, 'utf-8')
+    except NameError: # unicode is a default on python 3 
+        pass
+    text = unicodedata.normalize('NFD', text)
+    text = text.encode('ascii', 'ignore')
+    text = text.decode("utf-8")
+    return str(text)
+
+def simplify(text):
+    text = strip_accents(text.lower())
+    text = re.sub('[\n]', ' ', text)
+    text = re.sub('[^a-z ]', '', text)
+    return text
 
 def clean_txt(txt, prefix):
+    txt = simplify(txt)
     with open(prefix+"_clean.txt",'w') as out:
-        del_sym = [',', '.', ';', ':', '_', '¡', '!', '¿', '?', '(', ')', '$', '€', '*', '=', '%', '&', '/', '#', '·']
-        space_sym = ['-', '\n', '"', "'", '»', '«', '<', '>']
-        for c in txt:
-            if (c in del_sym): c = ''
-            elif (c in space_sym): c = ' '
-            
-            out.write(c.lower())
+        out.write(txt)
 
 
-def main(input="C:\Users\Xavi\Documents\FIB\CDI\Lab\Lab1\quijote.txt", filename="quijote"):
-    with open(input,'r') as file:
-        txt = file.read()
-    clean_txt(txt,filename)
+def main(input="", filename=""):
+    if (input == "" or filename == ""):
+        print("Usage: Lab1.py path/to/txt filename\n")
+    else:
+        with open(input,'r') as file:
+            txt = file.read()
+        clean_txt(txt,filename)
 
 
 
