dc6b1b10c343b21a4e69d1a81f54cc173aad029b
Xavi <antiwindows17@gmail.com>
2016-02-24 17:18:15 +0100

    clean + entropy inicial

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -3,46 +3,61 @@
 Javier Garcia Sanchez
 """
 import re
+import string
 import sys
 import unicodedata
-import string
+
+from numpy import log2
+
+
+def count_letter(txt, c):
+    return sum(l == c for l in txt)
+
+def count_total(txt):
+    return sum(c != ' ' for c in txt)
+
+def info(p):
+    return log2(1.0 / p)          #no usar probabilitats reals dona divisio per 0
+
+def entropy(txt):
+    total_letters = count_total(txt)
+    accum = 0.0
+    for l in string.ascii_lowercase:
+        p = count_letter(txt, l) / total_letters
+        accum += p * info(p)
+    return accum
+
+
+#########################################################
 
 def remove_accents(txt):
     txt = unicode(txt, 'utf-8')
-    return ''.join(x for x in unicodedata.normalize('NFKD', txt) if x in string.ascii_letters or x in string.whitespace).lower()
+    return ''.join(
+        x for x in unicodedata.normalize('NFKD', txt) if x in string.ascii_letters or x in string.whitespace).lower()
 
-
-'''def strip_accents(text):
-    try:
-        text = unicode(text, 'utf-8')
-    except NameError:  # unicode is a default on python 3
-        pass
-    text = unicodedata.normalize('NFKD', text)
-    text = text.encode('ascii', 'ignore')
-    text = text.decode("utf-8")
-    return str(text)
-
-
-def simplify(text):
-    text = strip_accents(text.lower())
-    text = re.sub('[\n]', ' ', text)
-    text = re.sub('[^a-z ]', '', text)
-    return text
-'''
 
 def clean_txt(txt, prefix):
     txt = remove_accents(txt)
+    txt = re.sub('[\n]', ' ', txt)
     with open(prefix + "_clean.txt", 'w') as out:
         out.write(txt)
 
 
-def main(input="", txtname=""):
-    if input == "" or txtname == "":
-        print("Usage: Lab1.py path/to/txt name_of_text\n")
-    else:
+#########################################################
+
+def main(case="", input="", txtname=""):
+    if case == "clean" and input != "" and txtname != "":
         with open(input, 'r') as file:
             txt = file.read()
         clean_txt(txt, txtname)
+    elif case == "entropy" and input != "":
+        with open(input, 'r') as file:
+            txt = file.read()
+        print "H(x): " + entropy(txt)
+    else:
+        print("Usage:")
+        print("Lab1.py clean path/to/txt name_of_text")
+        print("Lab1.py entropy path/to/txt")
 
 
 if __name__ == '__main__':  # no tocar
