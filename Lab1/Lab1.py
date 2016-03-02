036ef554a7d81bac7dfe808e23b71ecdd2bb73f9
Xavi <antiwindows17@gmail.com>
2016-03-02 18:16:52 +0100

    joint

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -10,22 +10,33 @@ import unicodedata
 from numpy.lib.scimath import log2
 
 
-def total_consecutive(txt):                      #total de parells consecutius
-    pass
+def total_consecutive(txt):       # total de parells consecutius
+    total = 0
+    for i in range(0, len(txt) - 1):
+        if txt[i] != ' ' and txt[i + 1] != ' ': total += 1
+    return total
 
-def count_consecutive(txt, par):                 #aparicions de parell "par" en el text
-    pass
+
+def count_consecutive(txt, par):  # aparicions de parell "par" en el text
+    ret = 0
+    for i in range(0, len(txt) - 1):
+        if txt[i] == par[0] and txt[i + 1] == par[1]: ret += 1
+    return float(ret)
+
 
 def count_letter(txt, c):
     ret = sum(l == c for l in txt)
     if ret > 0:
         return float(ret)
 
+
 def count_total(txt):
     return sum(c != ' ' for c in txt)
 
+
 def info(p):
     return log2(1.0 / p)
+
 
 def entropy(txt):
     total_letters = count_total(txt)
@@ -35,11 +46,30 @@ def entropy(txt):
         if count > 0:
             p = count / total_letters
             accum += p * info(p)
-            #print l + " " + str(p)
+            # print l + " " + str(p)
     return accum
 
+
+def parells():
+    p = []
+    for i in string.ascii_lowercase:
+        for j in string.ascii_lowercase:
+            p.append(i + j)
+    return p
+
+
 def joint_entropy(txt):
-    pass
+    total_pairs = total_consecutive(txt)
+    accum = 0.0
+    pairs = parells()
+    for par in pairs:
+        count = count_consecutive(txt, par)
+        if count > 0:
+            p = count / total_pairs
+            accum += p * info(p)
+            print par + " " + str(p)
+    return accum
+
 
 #########################################################
 
@@ -57,7 +87,7 @@ def clean_txt(txt, prefix):
     txt = re.sub('[\n]', ' ', txt)
     with open(prefix + "_clean.txt", 'w') as out:
         out.write(txt)
-        #print set(txt)       #lletres resultants en el text netejat
+        # print set(txt)       #lletres resultants en el text netejat
 
 
 #########################################################
@@ -70,11 +100,16 @@ def main(case="", input="", txtname=""):
     elif case == "entropy" and input != "":
         with open(input, 'r') as file:
             txt = file.read()
-        print "H(x): " + str(entropy(txt))
+        print "H(X): " + str(entropy(txt))
+    elif case == "joint_entropy" and input != "":
+        with open(input, 'r') as file:
+            txt = file.read()
+        print "H(X,Y): " + str(joint_entropy(txt))
     else:
         print("Usage:")
-        print("Lab1.py clean path/to/txt name_of_text")
-        print("Lab1.py entropy path/to/txt")
+        print("Lab1.py clean path/to/originaltxt name_of_text")
+        print("Lab1.py entropy path/to/cleantxt")
+        print("Lab1.py joint_entropy path/to/cleantxt")
 
 
 if __name__ == '__main__':  # no tocar
