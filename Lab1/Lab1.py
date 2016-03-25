74a01f9506cbda64f7b0593ac6c4c993736636e8
Xavi <antiwindows17@gmail.com>
2016-03-25 17:27:16 +0100

    entrega

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Javier Garcia Sanchez
+47179375-G
 """
 import random as rng
 import re
@@ -12,7 +13,7 @@ from numpy.lib.scimath import log2
 
 
 # region auxiliars
-def pars(txt):
+def pars(txt):                    # llista de parells de lletres
     pairs = []
     for i in range(0, len(txt) - 1):
         if txt[i] != ' ' and txt[i + 1] != ' ': pairs.append(txt[i] + txt[i + 1])
@@ -44,7 +45,7 @@ def info(p):
     return log2(1.0 / p)
 
 
-def entropy(txt):
+def entropy(txt):                            # entropia de la variable X d'un text: lletra aleatoria del text
     total_letters = count_total(txt)
     accum = 0.0
     for l in string.ascii_lowercase:
@@ -52,13 +53,13 @@ def entropy(txt):
         if count > 0:
             p = count / total_letters
             accum += p * info(p)
-            print l + " " + str(p) + " " + str(count)
+            #print l + " " + str(p) + " " + str(count)
     return accum
 # endregion
 
 
 # region joint entropy
-def parells():
+def parells():   #llista de tots els parells possibles des de "aa" fins a "zz"
     p = []
     for i in string.ascii_lowercase:
         for j in string.ascii_lowercase:
@@ -66,8 +67,8 @@ def parells():
     return p
 
 
-def joint_entropy(txt):
-    total_pairs = total_consecutive(txt)
+def joint_entropy(txt):                          # entropia conjunta de dos variables aleatories
+    total_pairs = total_consecutive(txt)         # que representen un parell de lletres consecutives
     accum = 0.0
     pairs = parells()
     for par in pairs:
@@ -75,7 +76,7 @@ def joint_entropy(txt):
         if count > 0:
             p = count / total_pairs
             accum += p * info(p)
-            print par + " " + str(count) + " " + str(p)
+            #print par + " " + str(count) + " " + str(p)
     return accum
 # endregion
 
@@ -90,8 +91,8 @@ def after_letter(txt, ltr, i):
         return 0
 
 
-def conditional_entropy1(txt, ltr):
-    if ltr == "espai":
+def conditional_entropy1(txt, ltr):              # entropia condicional respecte una lletra fixada "ltr"
+    if ltr == "espai":                           # entropia d'una lletra del text si ve després de "ltr"
         count = float(txt.count(" "))
         if count > 0:
             p_ltr = count / len(txt)
@@ -114,8 +115,8 @@ def conditional_entropy1(txt, ltr):
     return accum
 
 
-def conditional_entropy(txt):
-    total_pairs = total_consecutive(txt)
+def conditional_entropy(txt):                    # entropia condicional d'una lletra Y respecte X per
+    total_pairs = total_consecutive(txt)         # a tots els parells Y,X del text donat
     letters = count_total(txt)
     accum = 0.0
     for i in string.ascii_lowercase:                 #lletra X
@@ -131,8 +132,8 @@ def conditional_entropy(txt):
 
 
 '''
-def conditional_entropy(txt):
-    letters = count_total(txt)
+def conditional_entropy(txt):                    # implementació alternativa del calcul de entropia
+    letters = count_total(txt)                   # condicional H(Y|X) (més costós)
     accum = 0.0
     for i in string.ascii_lowercase:                 #lletra X
         for j in string.ascii_lowercase:             #lletra Y
@@ -166,8 +167,8 @@ def clean_txt(txt, prefix):
 
 
 # region text generation
-def new_text(nom, txt):
-    nou = ""
+def new_text(nom, txt):                                 #text random generat amb les mateixes probilitats
+    nou = ""                                            #de lletres que el text donat, i per tant, mateixa entropia
     for i in range(0, len(txt)):
         nou += rng.choice(txt)
     nou = re.sub(' +', ' ', nou)
@@ -224,3 +225,25 @@ def main(case="", input="", aux=""):
 
 if __name__ == '__main__':  # no tocar
     main(*sys.argv[1:])
+
+'''
+He utilitzat 2 textos en català, 2 en anglès, 1 en castellà (el quijote) i 1 en alemany.
+La majoria de les proves les he fet finalment amb un dels textos en català (Tirant lo Blanc) i el text
+en castellà (el quijote) perque son els dos mes llargs que he obtingut (sobre els 2 milions de caracters cadascun).
+Entropia de "El Quijote": 4.0304
+Entropia "Tirant lo Blanc": 4.0231
+Entropia "Also sprach Zarathustra": 4.0415
+Entropia "Oliver Twist": 4.1812
+Entropia "Ulysses": 4.2051
+En general sembla que l'anglès té una mica més d'informació (entropia) que el castellà, català o alemany
+
+A l'hora de generar nous textos aleatoris cal remarcar que per a poder generar un text amb la mateixa entropia conjunta,
+aquest es genera guardant directament els parells seleccionats aleatoriament del text base perque sino per a fer el calcul
+de la entropia conjunta del nou text es compten probabilitats de parells errònies:
+per exemple: si tenim un text "aabbcc" i generem un nou amb els parells extrets aleatòriament per exemple "aaaacc"
+en el primer text tenim: 1 "aa", 1 "ab", 1 "bb", 1 "bc" i 1 "cc" mentre que en el segon tindriem: 3 "aa" (quan en
+realitat hem inserit 2), 1 "ac"(que no existeix en el text base) i 1 "cc"
+Si guardem el text generat per parells inserint espais no tenim aquest problema "aa aa cc" pero estem introduint
+espais de manera forçada (tot i que el text base també té espais)
+
+'''
