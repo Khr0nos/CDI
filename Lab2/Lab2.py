ee862e2239800fdd75f030f8b7f3cc13a1e71094
xavi <antiwindows17@gmail.com>
2016-04-12 19:00:38 +0200

    codes

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -21,53 +21,75 @@ def shannon_code(src):
 
 
 def source_extension(src, k):
-    k_src = []
-
+    k_src = {}
+    claus = sorted(src.keys())
+    if k == 2:
+        for i in claus:
+            for j in claus:
+                k_src[i + j] = src[i] * src[j]
+    elif k == 3:
+        for i in claus:
+            for j in claus:
+                for k in claus:
+                    k_src[i + j + k] = src[i] * src[j] * src[k]
+    elif k == 4:
+        for i in claus:
+            for j in claus:
+                for k in claus:
+                    for l in claus:
+                        k_src[i + j + k + l] = src[i] * src[j] * src[k] * src[l]
     return k_src
 
 
+def source_fromstring_binary(txt):
+    length = len(txt)
+    src = {bit: float(txt.count(bit))/ length for bit in ["0", "1"]}
+    return src
+
 def source_fromstring(txt):
-    src = []
+    length = len(txt) - txt.count(" ")
+    src = {ltr: float(txt.count(ltr)) / length for ltr in string.ascii_lowercase if float(txt.count(ltr)) > 0}
+    '''src = {}
     length = len(txt) - txt.count(" ")
     for ltr in string.ascii_lowercase:
         count = float(txt.count(ltr))
         if count > 0:
             p = count / length
-            src.append((ltr, p))
+            src[ltr] = p'''
     return src
 
 
 def source_fromstring_weights(txt):
-    src = []
-    for ltr in string.ascii_lowercase:
+    src = {ltr: float(txt.count(ltr)) for ltr in string.ascii_lowercase if float(txt.count(ltr)) > 0}
+    '''for ltr in string.ascii_lowercase:
         count = float(txt.count(ltr))
         if count > 0:
-            src.append((ltr, count))
+            src[ltr] = count'''
     return src
 
 
 def entropy_source(src):
     accum = 0.0
-    for par in src:
-        if par[1] > 0:
-            accum += par[1] * log2(1.0 / par[1])
+    for k in sorted(src.keys()):
+        if src[k] > 0:
+            accum += src[k] * log2(1.0 / src[k])
     return accum
 
 
 def entropy_source_weights(src, l):  #l = suma de tots els pesos
     accum = 0.0
-    for par in src:
-        if par[1] > 0:
-            p = par[1] / l
+    for k in sorted(src.keys()):
+        if src[k] > 0:
+            p = src[k] / l
             accum += p * log2(1.0 / p)
     return accum
 
 
 def get_source(infile):
-    src = []
+    src = {}
     for line in infile:
         l = line.split(" ")
-        src.append((l[0], float(l[1])))
+        src[l[0]] = float(l[1])
     return src
 
 
@@ -76,15 +98,16 @@ def main(case="", input="", aux=""):
         if aux == "":
             src = source_fromstring(input)
             print "Source extracted\n" + str(src)
-        elif aux == "w":                            #flag "w" vol dir calcular la font amb weights no probabilitats
+        elif aux == "w":                            #flag "w" vol dir calcular la font amb weights, no probabilitats
             src = source_fromstring_weights(input)
             print "Source extracted\n" + str(src)
-        elif aux != "" and int(aux) > 0:
-            with open(input, 'r') as infile:
-                src = get_source(infile)
-                k = int(aux)
-                src = source_extension(src, k)
-                print "Source with " + str(k) + " extension\n" + str(src)
+        elif aux != "" and int(aux) > 0:            #k extension >= 2 i <= 4
+            '''with open(input, 'r') as infile:
+                src = get_source(infile)'''
+            src = source_fromstring(input)
+            k = int(aux)
+            src_k = source_extension(src, k)
+            print "Source with " + str(k) + " extension\n" + str(src_k)
     elif case == "entropy":
         with open(input, 'r') as infile:
             src = get_source(infile)
@@ -92,10 +115,19 @@ def main(case="", input="", aux=""):
                 print "H(S): " + str(entropy_source_weights(src, float(aux)))
             elif aux == "":
                 print "H(S): " + str(entropy_source(src))
+    elif case == "code":
+        src = source_fromstring_binary(input)
+        print src
+        if aux == "shannon":
+            print "Shannon code: " + str(shannon_code(src))
+        elif aux == "fano":
+            print "Shannon-Fano code: " + str(shannon_fano_code(src))
+        elif aux == "huffman":
+            print "Huffman code: " + str(huffman_code(src))
 
 
 if __name__ == '__main__':  # no tocar
     main(*sys.argv[1:])
 
 
-#quijote_clean.txt as source with length = 1640626
+#src1_weights.txt as source with length = 1640626
