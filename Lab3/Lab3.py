1563c76a4e7f0101df84c11e6c530c8f55e321df
Xavi <antiwindows17@gmail.com>
2016-04-17 22:03:39 +0200

    encoding...

--- Lab3/Lab3.py
+++ Lab3/Lab3.py
@@ -6,37 +6,57 @@ Javier Garcia Sanchez
 import string
 import sys
 
+def binary_bits(x, n):
+    bits = bin(x).split('b')[1]
+    return bits * n
+
+
+def numeric_val(x):
+    pass
 
 def source_fromstring(txt):
-    src = {ltr: int(txt.count(ltr)) for ltr in string.ascii_lowercase if int(txt.count(ltr)) > 0}
+    src = [(ltr, txt.count(ltr)) for ltr in string.ascii_lowercase if txt.count(ltr) > 0]
     return src
 
 
 def source_fromstring_binary(txt):
-    src = {bit: float(txt.count(bit)) for bit in ['0', '1']}
+    src = [(bit, txt.count(bit)) for bit in ['0', '1']]
     return src
 
 
-
-def arithmetic_encode(str, src, k):
-    return []
+def arithmetic_encode(txt, src, k):
+    c = ""
+    code = []
+    alpha = binary_bits(0, k)
+    beta = binary_bits(1, k)
+    alpha_val = int(alpha, 2)
+    beta_val = int(beta, 2)
+    delta = int(beta, 2) - int(alpha, 2) + 1
+    #print delta
+    #split into n subintervals
+    for i in range(len(txt)):
+        pass
+    return code
 
 
 def arithmetic_decode(bin_code, src, k, len):
     return ""
 
 
-def main(case="", font="", txt="", k="", l=""):
-    if font[0] in string.ascii_lowercase:
-        src = source_fromstring(font)
+def main(case="", txt="", k="", l="", code=""):
+    if txt[0] in string.ascii_lowercase:
+        src = source_fromstring(txt)
     else:
-        src = source_fromstring_binary(font)
+        src = source_fromstring_binary(txt)
     #print src
+    with open(txt + "_source.txt", 'w') as output:
+        for par in src:
+            output.write(str(par[0]) + " " + str(par[1]) + "\n")
     if case == "encode":
-        bin_code = arithmetic_encode(txt, src, int(k))
+        bin_code = arithmetic_encode(txt, src, int(k))        #txt == input string
         print bin_code
     else:
-        txt = arithmetic_decode(bin_code, src, k, l)
+        txt = arithmetic_decode(code, src, int(k), l)         #code == bin
         print txt
 
 
