ca299be5ec0ba3a364b80fc59fc245bf70b3668c
xavi <antiwindows17@gmail.com>
2016-04-19 18:56:25 +0200

    decoding...

--- Lab3/Lab3.py
+++ Lab3/Lab3.py
@@ -14,9 +14,6 @@ def binary_bits(x, n):
     return bits
 
 
-def numeric_val(x):
-    pass
-
 def source_fromstring(txt):
     src = [(ltr, txt.count(ltr)) for ltr in string.ascii_lowercase if txt.count(ltr) > 0]
     src2 = [(ltr, float(txt.count(ltr)) / len(txt)) for ltr in string.ascii_lowercase if txt.count(ltr) > 0]
@@ -33,8 +30,8 @@ def fill(str, char, u):
     return str + (char * u)
 
 def arithmetic_encode(txt, src, k, pi):
+    # initialization
     code = []
-    #initialization
     alpha = '0' * k
     beta = '1' * k
     u = 0
@@ -74,31 +71,74 @@ def arithmetic_encode(txt, src, k, pi):
     return code
 
 
-def arithmetic_decode(bin_code, src, k, len):
-    return ""
+def arithmetic_decode(bin_code, src, k, l, pi):
+    #initialization
+    txt = ""
+    alpha = '0' * k
+    beta = '1' * k
+    gamma = ""
+    for c in bin_code[:k]:
+        gamma += c
+    for i in range(l):
+        # split into n subintervals
+        delta = int(beta, 2) - int(alpha, 2) + 1
+        interval = []
+        for j in range(1, len(pi)):
+            interval.append((int(int(alpha, 2) + floor(delta * pi[j - 1])), int(int(alpha, 2) + floor(delta * pi[j] - 1))))
+        #find next letter
+        next = int(gamma, 2)
+        it = 0
+        for j in range(len(interval)):
+            if interval[j][0] <= next <= interval[j][1]:
+                it = j
+                break
+        txt += src[it][0]
+        alpha = binary_bits(interval[it][0], k)
+        beta = binary_bits(interval[it][1], k)
+        if len(txt) == l:
+            break
+        #rescaling
+
+    return txt
 
 
 def main(case="", txt="", k="", l="", code=""):
-    if txt[0] in string.ascii_lowercase:
-        src, src2 = source_fromstring(txt)
-    else:
-        src, src2 = source_fromstring_binary(txt)
-    pi = [0]
-    acc = 0.0
-    for i in range(len(src2)):
-        acc += src2[i][1]
-        pi.append(acc)
-    pi[len(src2)] = 1.0
-    #print pi
-    with open(txt + "_source.txt", 'w') as output:
-        for par in src:
-            output.write(str(par[0]) + " " + str(par[1]) + "\n")
-        #output.write(str(len(txt)))
     if case == "encode":
+        if txt[0] in string.ascii_lowercase:
+            src, src2 = source_fromstring(txt)
+        else:
+            src, src2 = source_fromstring_binary(txt)
+        pi = [0]
+        acc = 0.0
+        for i in range(len(src2)):
+            acc += src2[i][1]
+            pi.append(acc)
+        pi[len(src2)] = 1.0
+        # print pi
+        with open(txt + "_source.txt", 'w') as output:
+            for par in src:
+                output.write(str(par[0]) + " " + str(par[1]) + "\n")
+                # output.write(str(len(txt)))
         bin_code = arithmetic_encode(txt, src, int(k), pi)        #txt == input string
         print bin_code
     else:
-        txt = arithmetic_decode(code, src, int(k), l)         #code == bin
+        src = []
+        src2 = []
+        with open(txt, 'r') as infile:                            #txt == fitxer amb la font
+            for line in infile:
+                par = line.split(" ")
+                src.append((par[0], int(par[1])))
+                src2.append((par[0], float(par[1]) / int(l)))
+        pi = [0]
+        acc = 0.0
+        for i in range(len(src2)):
+            acc += src2[i][1]
+            pi.append(acc)
+        pi[len(src2)] = 1.0
+        #print src
+        #print src2
+        code = [c for c in code]
+        txt = arithmetic_decode(code, src, int(k), int(l), pi)         #code == bin
         print txt
 
 
