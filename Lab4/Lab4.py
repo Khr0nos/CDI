7259de244a3c61659634c6159e567126fc91bf98
Xavi <antiwindows17@gmail.com>
2016-04-26 17:41:16 +0200

    Lab4 init

---
+++ Lab4/Lab4.py
@@ -0,0 +1,52 @@
+# coding=utf-8
+"""
+Javier Garcia Sanchez
+47179375-G
+"""
+import sys
+
+def LZ77_encode():
+    pass
+
+def LZ77_decode():
+    pass
+
+def LZSS_encode():
+    pass
+
+def LZSS_decode():
+    pass
+
+def LZ78_encode():
+    pass
+
+def LZ78_decode():
+    pass
+
+def LZW_encode():
+    pass
+
+def LZW_decode():
+    pass
+
+
+def main(case="", txt="", s="", t="", m="", dic="", tok=""):
+    if case == "encode_lz77":
+        pass
+    elif case == "decode_lz77":
+        pass
+    elif case == "encode_lzss":
+        pass
+    elif case == "decode_lzss":
+        pass
+    elif case == "encode_lz78":
+        pass
+    elif case == "decode_lz78":
+        pass
+    elif case == "encode_lzw":
+        pass
+    elif case == "decode_lzw":
+        pass
+
+if __name__ == '__main__':  # no tocar
+    main(*sys.argv[1:])
