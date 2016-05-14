c00847f1b01b66a65656ae77e2765134671e080d
Xavi <antiwindows17@gmail.com>
2016-05-14 20:56:37 +0200

    Lab5

---
+++ Lab5/Lab5.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+"""
+Javier Garcia Sanchez
+47179375-G
+"""
+
+
+def main():
+    #misc.imread("C:\\Users\\Xavi\\Pictures\\Praga - Viena - Budapest\\CIMG0993.JPG")
+    pass
+
+
+
+
+
+
+
+
+if __name__ == '__main__':  # no tocar
+    main()
